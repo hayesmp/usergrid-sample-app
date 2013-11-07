257c9010dde393ed8435dc90ed9499f889c61b8f
Michael Hayes <tasslehawf@mac.com>
2013-11-07 10:14:36 -0600

    Initial Commit

---
+++ usergrid-sample-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  usergrid-sample-app
+//
+//  Created by Michael Hayes on 11/7/13.
+//  Copyright (c) 2013 MHayes Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ usergrid-sample-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  usergrid-sample-app
+//
+//  Created by Michael Hayes on 11/7/13.
+//  Copyright (c) 2013 MHayes Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
